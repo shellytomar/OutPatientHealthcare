5e63155ff033e835885456236df3ab40bae322c7
Shelly Tomar <shelly.tomar@gmail.com>
2017-04-25 13:54:44 -0700

    XML Files Modified

--- app/src/main/java/com/example/shell/outpatienthealthcare/model/BloodPressure.java
+++ app/src/main/java/com/example/shell/outpatienthealthcare/model/BloodPressure.java
@@ -1,14 +1,33 @@
 package com.example.shell.outpatienthealthcare.model;
 
+import java.io.Serializable;
+
 /**
  * Created by Shelly Tomar on 4/25/2017.
  */
 
-public class BloodPressure {
+public class BloodPressure implements Serializable {
     private int bpInfoID;
     private int systolicBP;
     private int diastolicBP;
     private String dateTime;
+
+    public void setBpInfoID(int bpInfoID) {
+        this.bpInfoID = bpInfoID;
+    }
+
+    public void setSystolicBP(int systolicBP) {
+        this.systolicBP = systolicBP;
+    }
+
+    public void setDiastolicBP(int diastolicBP) {
+        this.diastolicBP = diastolicBP;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
 
     public int getBpInfoID() {
         return bpInfoID;
